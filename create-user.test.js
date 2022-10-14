c8656fb6007068bfba946e30aeba8b7b2f8e1cb4
nanashu10 <antoniusbrian1@gmail.com>
2022-10-14 15:15:57 -0700

    first commit

---
+++ create-user.test.js
@@ -0,0 +1,14 @@
+it('Succsessfuly create new user', () => {
+    var user = {
+        "name": 'Brian',
+        "job": 'Test Engineer'
+    } 
+    cy.request('POST', 'https:reqres.in/api/users', user).then((response) => {
+        expect(response.status).equal(201)
+        expect(response.body.name).to.eq(user.name)
+        expect(response.body.job).to.eq(user.job)
+
+
+        
+    })
+})
