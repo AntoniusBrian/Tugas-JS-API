06d5b9faf6887667435dac3df85aa843c17fa8f7
nanashu10 <antoniusbrian1@gmail.com>
2022-10-15 16:09:39 -0700

    first commit

---
+++ login-user.test.js
@@ -0,0 +1,19 @@
+// cypress/integration/programmaticLogin.spec.js
+
+it('successfully logs in programmatically', () => {
+    cy.intercept('GET', `${Cypress.env('apiUrl')}/models?userId=*`)
+      .as('getUserModels')
+  
+    cy.request('POST', `${Cypress.env('apiUrl')}/users/login`, {
+      username: Cypress.env('userEmail'),
+      password: Cypress.env('userPassword'),
+    }).then((response) => {
+      cy.setCookie('sessionId', response.body.sessionId)
+      cy.setCookie('userId', response.body.userId)
+      cy.setCookie('userName', response.body.userName)
+    })
+  
+    cy.visit('/#!/main')
+    cy.wait('@getUserModels')
+    cy.contains('h2', 'Models').should('be.visible')
+  })
